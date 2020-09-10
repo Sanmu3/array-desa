b9cea1929319248aa11ea945af4aa4306385c4ff
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-10 22:53:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var listDesaBantul = [
+    ['argodadi', 'sedayu', 'bantul'],
+    ['argomulyo' , 'sedayu', 'bantul'],
+    ['argorejo', 'sedayu', 'bantul'],
+    ['argosari', 'sedayu', 'bantul'],
+
+    //B
+
+    ['bangunjiwo', 'kasihan', 'bantul'],
+    ['banguntapan', 'banguntapan', 'bantul'],
+    ['bantul', 'bantul', 'bantul'],
+    ['baturetno', 'banguntapan', 'bantul'],
+    ['bawuran', 'pleret', 'bantul'],
+
+    //c
+
+    ['canden', 'jetis', 'bantul'],
+    ['caturharjo', 'pandak', 'bantul'],
+    ['cetan srigading']
+]
+console.log(listDesaBantul[0])
+console.log(listDesaBantul[1])
+console.log(listDesaBantul[2])
+console.log(listDesaBantul[3])
+console.log(listDesaBantul[4])
+console.log(listDesaBantul[5])
+console.log(listDesaBantul[6])
+console.log(listDesaBantul[7])
+console.log(listDesaBantul[8])
+console.log(listDesaBantul[9])
+console.log(listDesaBantul[10])
+console.log(listDesaBantul[11])
